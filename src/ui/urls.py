dc426c398beebbf15e396154e04e3de813454f46
yunussozeri <yunus.sozeri.254@gmail.com>
2024-11-11 11:44:54 +0100

    REST API endpoints not working

--- src/ui/urls.py
+++ src/ui/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("hello", views.home_view, name="home"),
+    path('api/example/', views.example_view),
 ]
--- src/ui/views.py
+++ src/ui/views.py
@@ -1,6 +1,15 @@
 # views.py
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from django.shortcuts import render
 import requests
 
 def home_view(request):
-    return 
+    data = {"message": "Hello, World!"}
+    return Response(data, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def example_view(request):
+    data = {"message": "Hello, World!"}
+    return Response(data, status=status.HTTP_200_OK)
