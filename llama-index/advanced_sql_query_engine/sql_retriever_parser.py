9353365e1453cd8e985e4d28bec3ebb4b05a1fc4
benedikt-weyer <benedikt.oskar.weyer@gmail.com>
2024-12-02 11:14:35 +0100

    add sql retriever and parser

---
+++ llama-index/advanced_sql_query_engine/sql_retriever_parser.py
@@ -0,0 +1,24 @@
+from llama_index.core.retrievers import SQLRetriever
+from typing import List
+from llama_index.core.query_pipeline import FnComponent
+
+sql_retriever = SQLRetriever(sql_database)
+
+
+def get_table_context_str(table_schema_objs: List[SQLTableSchema]):
+    """Get table context string."""
+    context_strs = []
+    for table_schema_obj in table_schema_objs:
+        table_info = sql_database.get_single_table_info(
+            table_schema_obj.table_name
+        )
+        if table_schema_obj.context_str:
+            table_opt_context = " The table description is: "
+            table_opt_context += table_schema_obj.context_str
+            table_info += table_opt_context
+
+        context_strs.append(table_info)
+    return "\n\n".join(context_strs)
+
+def get_table_parser_component():
+    return FnComponent(fn=get_table_context_str)
