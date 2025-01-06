e3622d4a2a3eec5ba87784a534c6116eb74fb935
Benedikt Weyer <105204092+benedikt-weyer@users.noreply.github.com>
2025-01-06 12:15:53 +0100

    Added new example query

--- llm-in-container/local_run_query_engine.py
+++ llm-in-container/local_run_query_engine.py
@@ -1,6 +1,7 @@
 from advanced_sql_query_engine import submit_query
 
-response = submit_query("What districts are in the data?", True)
+#response = submit_query("What districts are in the data?", True)
+response = submit_query("How was the percipitation in Korle Klottey Municipal in 2020?", True)
 
 print(response)
 
