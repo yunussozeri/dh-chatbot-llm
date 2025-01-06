9586d9c0adc6f9dead4c092b8f130a1f4ae9f796
Benedikt Weyer <105204092+benedikt-weyer@users.noreply.github.com>
2025-01-06 12:49:13 +0100

    Revert "43 sql query syntax enhancement"

--- llm-in-container/local_run_query_engine.py
+++ llm-in-container/local_run_query_engine.py
@@ -1,9 +1,5 @@
 from advanced_sql_query_engine import submit_query
 
-#response = submit_query("What districts are in the data?", True)
-response = submit_query("How was the percipitation in Korle Klottey Municipal in 2020?", True)
+response = submit_query("What districts are in the data?", True)
 
 print(response)
-
-while True:
-    a=0
